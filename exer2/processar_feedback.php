2a94350c54f251741d15df554fe21639450873d5
Gabriel-Britoo <gabrito343@gmail.com>
2024-09-24 15:20:07 -0300

    Fim da minha page

--- exer2/processar_feedback.php
+++ exer2/processar_feedback.php
@@ -4,7 +4,18 @@ $avaliacao = $_POST['avaliacao'];
 $comentario = $_POST['comentario'];
 
 if($produto != 'selecionar' && $avaliacao != 'selecionar'){
-    echo "$produto <br> $avaliacao <br> $comentario";
+    echo "<div id='show-feedback'>
+            <h1 id='obrigado'>Obrigado por compartilhar!</h1>
+            <div id='texto-resultado'>
+                <p class='p-resultado-feedback'>Produto: $produto<p>
+                <p class='p-resultado-feedback'>Avaliação: $avaliacao<p>
+                <p class='p-resultado-feedback'>\"$comentario\"<p>
+            </div>
+          <div>";
+}
+else{
+    echo "<script>alert('Preencha todos os campos');
+    location.replace('feedback.php');</script>";
 }
 ?>
 
@@ -13,7 +24,9 @@ if($produto != 'selecionar' && $avaliacao != 'selecionar'){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./feedback.css">
+    <title>Feedback</title>
 </head>
 <body>
     
